e43f546fc86e2749be1ec572e6009ab69aa261c2
mambru <ecastro82@gmail.com>
2020-12-16 08:45:45 -0600

    basic player and enemy health and attack features

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,9 +10,36 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
-
+// creates the function
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 }
+//Subtract the value of 'playerAttack' from the value of `enemyHealth 
+enemyHealth = enemyHealth - playerAttack;
+//Log a resulting message to the console so we know that it worked.
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+//Subtract the value of `enemyAttack` from the value of `playerHealth`
+playerHealth = playerHealth - enemyAttack;
+//Log a resulting message to the console so we know that it worked.
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+//check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
 
+// executes the function
 fight();
