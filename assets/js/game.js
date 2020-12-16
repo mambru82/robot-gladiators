fea177b31a5a7560c948cbb921be6ace79eb1bf1
mambru <ecastro82@gmail.com>
2020-12-15 22:15:52 -0600

    added player health and attack points

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,18 @@
 // this creates a function named "fight"
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
+// You can also log multiple values at once like this
 
-// this will do math and log 20
-console.log(10+10);
-//what is this?
-console.log("Our robot's name is " + playerName + "!")
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-    window.alert("The fight has begun!");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
 }
 
-//fight();
+fight();
