190f70812ea8e4bb6eea9c75881c8339d10f3757
mambru <ecastro82@gmail.com>
2020-12-16 14:10:48 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+//Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//      * fight all enemy robots
+//      * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
 // declares the variables
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -5,15 +11,15 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
+//console.log(playerName, playerAttack, playerHealth);
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 //creates the function
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -24,7 +30,7 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
 enemyHealth = enemyHealth - playerAttack;
 //Log a resulting message to the console so we know that it worked.
 console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyName  + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
 //check enemy's health
 if (enemyHealth <= 0) {
@@ -37,7 +43,7 @@ else {
 playerHealth = playerHealth - enemyAttack;
 //Log a resulting message to the console so we know that it worked.
 console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
 );
 //check player's health
 if (playerHealth <= 0) {
@@ -65,4 +71,6 @@ else {
     }
 }
 // executes the function
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
