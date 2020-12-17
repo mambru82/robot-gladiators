3333be7da79d823ee8abee2e84199fea6674612d
mambru <ecastro82@gmail.com>
2020-12-17 09:26:45 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -61,7 +61,6 @@ var fight = function(enemyName) {
         //check player's health
         if (playerHealth <= 0) {
             window.alert(playerName + " has died!");
-            break
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
             }
@@ -70,7 +69,12 @@ var fight = function(enemyName) {
 };
 // executes the function
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    } else {
+    window.alert("You have lost your robot in battle! Game Over Bitches!");
+    break;}
 }
